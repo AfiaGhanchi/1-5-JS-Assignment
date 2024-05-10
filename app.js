b00716f4384ff130e1402f8b0e73b998ec726a5f
AfiaGhanchi <afiaghanchi77@gmail.com>
2024-05-10 23:39:36 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+alert("Error! please enter your valid password");
+alert("Welcome to JS Land\nHappy Coding!");
+alert("Welcome to JS Land...");
+alert("Happy Coding!\n");
+alert("Hello... I can run JS through my web browser's console");
+// alert sec.....................................close
+
+var userName = ("Username");
+var myName = ("Afia Ghanchi");
+console.log(userName);
+console.log(myName);
+var message = ("Hello World!");
+console.log(message);
+
+
+// Variable for string
+
+
+alert("John Doe");
+alert("15 years old");
+alert("Certified Moobile Application Development");
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+alert("My email address is afiaghanchi77@gmail.com");
+
+alert("I m trying to learn the Book A smarter\nway to learn javaScript");
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+
